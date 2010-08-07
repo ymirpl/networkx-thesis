39ac110a9256cb0d1b91e13eda307182f2084298
Marcin Mincer <marcin.mincer@gmail.com>
2010-08-07 12:57:34 +0200

    first commit

---
+++ src/main.py
@@ -0,0 +1,9 @@
+'''
+Created on 02-08-2010
+
+@author: ymir
+'''
+from thesis import *
+
+if __name__ == '__main__':
+    pass
---
+++ src/thesis/sna.py
@@ -0,0 +1,17 @@
+'''
+Created on 03-08-2010
+
+@author: ymir
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        pass
