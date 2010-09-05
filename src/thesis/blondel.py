5e58360c8680992520ab46d6e67cc0b30d8546ad
Marcin Mincer <marcin.mincer@gmail.com>
2010-09-05 21:10:28 +0200

    end of day

--- src/thesis/blondel.py
+++ src/thesis/blondel.py
@@ -8,12 +8,12 @@ class BlondelClusterer(object):
         # initially every node is in different cluster
         for nodeNo in xrange(1, self.G.number_of_nodes()+1):
             self.G.node[nodeNo]['cluster'] = nodeNo
-        self.computeQ()
+        self.Q = self.computeQ()
         self.clusters = []
 
     def computeQ(self):
         
-        m = 0.5 * sum([data['weight'] for startNode, endNode, data in self.G.edges_iter(data=True)])
+        self.m = 0.5 * sum([data['weight'] for startNode, endNode, data in self.G.edges_iter(data=True)])
         
         total = 0.0
         
@@ -24,23 +24,55 @@ class BlondelClusterer(object):
                 else:
                     Aij = self.G[nodeNo][secondNodeNo]['weight']
                     
-                    def sumOfAdjacentEdges(nodeNo):
-                        sum = 0.0
-                        for key, value in self.G[nodeNo].iteritems():
-                            if key == 'cluster':
-                                continue
-                            else:
-                                sum += value['weight']
-                        return sum
+
                     
-                    ki = sumOfAdjacentEdges(nodeNo)
-                    kj = sumOfAdjacentEdges(secondNodeNo)
+                    ki = self.sumOfAdjacentEdges(nodeNo)
+                    kj = self.sumOfAdjacentEdges(secondNodeNo)
                     
-                    total += Aij - ki*kj/2*m
+                    total += Aij - ki*kj/2*self.m
                     
         return total
     
         
+    def sumOfAdjacentEdges(self, nodeNo, cluster = None):
+        sum = 0.0
+        for key, value in self.G[nodeNo].iteritems():
+                if not cluster or self.G.node[key]['cluster'] == cluster:
+                    sum += value['weight']
+        return sum
+
+
+    def phaseOne(self):
+        for node, data in self.G.nodes_iter(data=True):
+            for buddyNode in self.G[node].iterkeys():
+                oldCluster = data['cluster']
+                data['cluster'] = self.G.node[buddyNode]['cluster'] # join buddy's cluster
+                q = self.computeQ() 
+            if q > self.Q:
+                self.Q = q
+            else:
+                data['cluster'] = oldCluster
+    
+    
+    
+    
+############################################################################    
+    # currently deprecated
+    def sumOfWeightsInsideCluster(self, cluster):
+        copyG = self.G.copy()
+        for node, data in copyG.nodes_iter(data=True):
+            if data['cluster'] != cluster:
+                copyG.remove_node(node)
+        
+        return sum([data['weight'] for startNode, endNode, data in copyG.edges_iter(data=True)])
+        
+    # TODO depracated, it may occur faster to compute basic Q again
+    def computeDeltaQ(self, node, cluster):
+        ki = self.sumOfAdjacentEdges(node)
+        ki_in = self.sumOfAdjacentEdges(node, cluster)
+        Ein = self.sumOfWeightsInsideCluster(cluster)
+        
+                
 def sigma(cluster1, cluster2):
     if cluster1 == cluster2:
         return 1
--- src/thesis/test_blondel.py
+++ src/thesis/test_blondel.py
@@ -10,31 +10,39 @@ import networkx as nx
 class SimpleTestCase(unittest.TestCase):
     def setUp(self):
         from lib import karate_graph
-        G = karate_graph.karate_graph()
-        self.G = G
-    
-    def testComputeQ(self):
+#        G = karate_graph.karate_graph()
+#        self.G = G
+        
         self.G = nx.Graph()
         self.G = nx.path_graph(5)
         self.G.remove_node(0)
         for (startNode, endNode, data) in self.G.edges(data=True):
             data['weight'] = 1
-        
+            
+        self.B = blondel.BlondelClusterer(self.G)
+    
+    def testComputeQ(self):
         import matplotlib.pyplot as plt
-        pos = nx.graphviz_layout(self.G, prog='neato')
-        nx.draw(self.G, pos)
+#        pos = nx.graphviz_layout(self.G, prog='neato')
+#        nx.draw(self.G, pos)
  
-        B = blondel.BlondelClusterer(self.G)
-        Q = B.computeQ()
+        
+        Q = self.B.computeQ()
         
 #        plt.show()
 #        print Q
         
         self.assertEqual(Q, 0)
-    
-#    def test(self):
-#        partition = blondel.cluster(self.G)
-#        # returns list o list of partiotions
+        
+        
+    def testPhaseOne(self):
+        self.B.phaseOne()
+        
+        for tuple in self.G.nodes_iter(data=True):
+            print tuple
+        
+        
+
         
         
 def test_main():
