14d7aa67e7d50a20f9d269a9ce3cadc8edb4207d
Marcin Mincer <marcin.mincer@gmail.com>
2010-09-05 18:51:29 +0200

    computeQ

--- src/main.py
+++ src/main.py
@@ -7,23 +7,26 @@ Created on 02-08-2010
 '''
 import networkx as nx
 from thesis import sna
+from thesis import test_blondel
+
 
 if __name__ == '__main__':
+    test_blondel.test_main()
     
 
-    dm = sna.DataMaker("/home/ymir/eclipse/networkx-thesis/testing-ring.txt")
-    dm.generate()
-
-    gm = sna.GraphMaker("/home/ymir/eclipse/networkx-thesis/testing-ring.txt")
-    gm.makeGraph()
-    
-    cl = sna.Cliquer(gm.graph)
-    cl.sliceGraph(3)
-#    cl.nativeCliquer()
-#    cl = sna.Cliquer(nx.erdos_renyi_graph(30, 0.05))
-#    pp = cl.blodelAlgorithm()
-#    cl.prettyPlotter(d=pp)
-#    pp = cl.newmanAlgorithm()
-#    cl.prettyPlotter(l=pp, filename="n.png")
-    pp = cl.aaronNewmanAlgorithm()
-    cl.prettyPlotter(l=pp)
+#    dm = sna.DataMaker("/home/ymir/eclipse/networkx-thesis/testing-ring.txt")
+#    dm.generate()
+#
+#    gm = sna.GraphMaker("/home/ymir/eclipse/networkx-thesis/testing-ring.txt")
+#    gm.makeGraph()
+#    
+#    cl = sna.Cliquer(gm.graph)
+#    cl.sliceGraph(3)
+##    cl.nativeCliquer()
+##    cl = sna.Cliquer(nx.erdos_renyi_graph(30, 0.05))
+##    pp = cl.blodelAlgorithm()
+##    cl.prettyPlotter(d=pp)
+##    pp = cl.newmanAlgorithm()
+##    cl.prettyPlotter(l=pp, filename="n.png")
+#    pp = cl.aaronNewmanAlgorithm()
+#    cl.prettyPlotter(l=pp)
