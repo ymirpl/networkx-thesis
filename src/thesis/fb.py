1cefe96a6fdd5382654669d9f9c706df2f91e316
Marcin Mincer <marcin.mincer@gmail.com>
2010-11-03 18:28:13 +0100

    exp 5

--- src/thesis/fb.py
+++ src/thesis/fb.py
@@ -1,7 +1,3 @@
-from simplejson import *
-from urllib2 import *
-from xml.dom import minidom
-from xml import dom
 import networkx as nx
 
 FB_API_KEY = "ca00488bed6f966941788ddca65823ca"
@@ -14,22 +10,26 @@ TOKEN = "152284658119481|917aefd01a1c2bac3e1d478f-1244349170|oejQMHFngbBNJCdGUXF
 class Facebooker:
     
     graph = nx.Graph()
-    myId = 0
+    myId = '0'
     
     def __init__(self):
         self.graph.add_node(self.myId, {'name': 'Marcin Mincer'})
     
     def fetchGraph(self):
+        from simplejson import loads
+        from urllib2 import urlopen
+        from xml.dom import minidom
+        
         friends = loads(urlopen('https://graph.facebook.com/me/friends?access_token='+TOKEN).read())
     
         for friend in friends['data']:
-            self.graph.add_node(int(friend['id']), {'name': friend['name']})
-            self.graph.add_edge(self.myId, int(friend['id']))
+            self.graph.add_node(friend['id'], {'name': friend['name']})
+            self.graph.add_edge(self.myId, friend['id'])
             
             
             foaf  = minidom.parse(urlopen('https://api.facebook.com/method/friends.getMutualFriends?target_uid='+friend['id']+'&source_uid=1244349170&access_token='+TOKEN))
             for f in foaf.getElementsByTagName("uid"):
-                self.graph.add_edge(int(f.firstChild.nodeValue), int(friend['id']))
+                self.graph.add_edge(f.firstChild.nodeValue, friend['id'])
                 
         self.saveGraph()
                 
@@ -38,15 +38,26 @@ class Facebooker:
         
     def loadGraph(self, filename = 'fb.gpickle'):
         self.graph = nx.read_gpickle(filename)
+        
+        # ok, redo this shitty graph
+        
+        
     
     def draw(self):
-        import matplotlib.pyplot as plt
-        nx.draw(self.graph)
-        plt.show()
+#        from pygraphviz import *
+#        A = nx.to_agraph(self.graph)
+#        A.draw('star.png',prog="circo")
+#        import matplotlib.pyplot as plt
+#        nx.draw(self.graph)
+#        plt.show()
+        dict = nx.closeness_vitality(self.graph, False, True)
+        print dict
             
 
 if __name__ == "__main__":
     fb = Facebooker()
+#    fb.fetchGraph()
+#    fb.saveGraph()
     fb.loadGraph()
     fb.draw()
 
